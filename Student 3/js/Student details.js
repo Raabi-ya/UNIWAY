59cdfca50cc4c4696870a27bef28f2930df116a1
Raabiya Rismy <136999950+Raabi-ya@users.noreply.github.com>
2024-05-26 01:07:33 +0530

    Add files via upload

---
+++ Student 3/js/Student details.js
@@ -0,0 +1,33 @@
+function showRaabiya() {
+    document.getElementById("raabiya").style.opacity="1";
+}
+function stopRaabiya(){
+    document.getElementById("raabiya").style.opacity="0";
+}
+function showAsanga(){
+    document.getElementById("asanga").style.opacity="1";
+}
+function stopAsanga(){
+    document.getElementById("asanga").style.opacity="0";
+
+}
+function showSenadi(){
+    document.getElementById("senadi").style.opacity="1";
+
+}
+function stopSenadi(){
+    document.getElementById("senadi").style.opacity="0";
+}
+function showZainab(){
+    document.getElementById("zainab").style.opacity="1";
+}
+function stopZainab(){
+    document.getElementById("zainab").style.opacity="0";
+}
+window.onload=()=>{
+    stopRaabiya();
+    stopAsanga();
+    stopSenadi();
+    stopZainab();
+}
+    
